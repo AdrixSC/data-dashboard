812b934713fd71f914ea8d5c290378167f47478f
Adriana Becerril <Adriana@MacBook-Air-de-Arisbeth.local>
2017-12-19 10:41:05 -0600

    funcion del submenu lista

--- js/app.js
+++ js/app.js
@@ -1,6 +1,20 @@
-/*
- * Funcionalidad de tu producto
- */
+//Funcion para el submenú que mostrará las opciones de generación a elegir
+var subMenuSedes = document.getElementsByClassName("submenu-sedes");
+for (var i = 0; i < subMenuSedes.length; i++) {
+    subMenuSedes[i].addEventListener("click", showMenu)
+}
 
-// Puedes hacer uso de la base de datos a través de la variable `data`
-//console.log(data);
+function showMenu() {
+    var subMenuGen = this.getElementsByClassName("submenu-gen")[0];
+    console.log(subMenuGen);
+
+    if (subMenuGen.classList.contains("hide")) {
+        subMenuGen.classList.remove("hide");
+        subMenuGen.classList.add("show");
+        console.log("subMenuGen");
+    } else {
+        subMenuGen.classList.remove("show");
+        subMenuGen.classList.add("hide");
+        console.log(subMenuGen);
+    }
+}
