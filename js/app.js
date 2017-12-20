e0f0064d0c069bb59755920c95e3a7b831a22f86
Adriana Becerril <Adriana@MacBook-Air-de-Arisbeth.local>
2017-12-20 03:52:19 -0600

    agregando estilos, e intentos de funciones

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 //Accediendo a la data
+
 //Total de estudiantes por sede y generación
 function totalStudents() {
     var studentsAQP1 = data.AQP["2016-2"].students;
@@ -143,6 +144,7 @@ function showMenu() {
     }
 }
 
+// Función para acceder a los datos desde el submenú
 function hola() {
     console.log(event.target);
     var target = event.target;
@@ -153,30 +155,9 @@ function hola() {
     console.log(data[sede]);
 }
 
-
-//Funcion para el submenú que mostrará las opciones de generación a elegir
-var subMenuSedes = document.getElementsByClassName("submenu-sedes");
-for (var i = 0; i < subMenuSedes.length; i++) {
-    subMenuSedes[i].addEventListener("click", showMenu)
-}
-
-function showMenu() {
-    var subMenuGen = this.getElementsByClassName("submenu-gen")[0];
-    console.log(subMenuGen);
-
-    if (subMenuGen.classList.contains("hide")) {
-        subMenuGen.classList.remove("hide");
-        subMenuGen.classList.add("show");
-        console.log("subMenuGen");
-    } else {
-        subMenuGen.classList.remove("show");
-        subMenuGen.classList.add("hide");
-        console.log(subMenuGen);
-    }
-}
-
-// submenu
-
+//SECCIÓN DASHBOARD
+// Funciones para mostrar el dashboard por generación
+//intento 1
 /*var showDash = document.getElementsById("are1602");
 //for (var i = 0; i < showDash.length; i++) {
 showDash.addEventListener("click", showImg)
@@ -186,18 +167,53 @@ function showImg() {
     var dashAqp1 = this.getElementsByClassName("aqp-dash-1");
     console.log(dashAqp1);
 
-    if (dashAqp1.classList.contains("show")) {
-        dashAqp1.classList.remove("show");
-        dashAqp1.classList.add("hide");
-        console.log("dashAqp1");
-    } else {
+    if (dashAqp1.classList.contains("hide")) {
         dashAqp1.classList.remove("hide");
         dashAqp1.classList.add("show");
+        console.log("dashAqp1");
+    } else {
+        dashAqp1.classList.remove("show);
+        dashAqp1.classList.add("hide");
         console.log(dashAqp1);
     }
 }*/
 
-//Nueva funcionalidad
+//intento 2
+/*function myFunctionDash1() {
+    var x = document.getElementById("d1").innerHTML;
+    document.getElementById("d1").innerHTML = '<img src="assets/images/AQP_2016-2.jpg"></img>';
+    if (x.classList.contains("hide")) {
+        x.classList.remove("hide");
+        x.classList.add("show")
+    } else {
+        x.classList.remove("show");
+        x.classList.add("hide");
+    }
+}
+
+function myFunctionDash2() {
+    var x = document.getElementById("d2").innerHTML;
+    document.getElementById("d2").innerHTML = '<img src="assets/images/AQP_2017-1.jpg"></img>';
+}*/
+
+//intento 3
+/*var drawDashScl1702 = document.getElementById('scl1702');
+drawDashScl1702.onclick = function() {
+    scl172.style.display = "block";
+    aqp162.style.display = "none";
+    /*informationStudentScl.style.display = "none";
+    informationStudentLim2.style.display = "none";
+    informationStudentLim1.style.display = "none";
+    informationStudentLim.style.display = "none";
+    informationStudentCdmx2.style.display = "none";
+    informationStudentCdmx.style.display = "none";
+    informationStudentArequipa1.style.display = "none";
+    informationStudentArequipa.style.display = "none";
+}*/
+
+
+//SECCIÓN ESTUDIANTES
+//Eventos para mostrar los perfiles de las estudiantes
 var wrapperAre1602 = document.getElementById('are1602');
 wrapperAre1602.onclick = function() {
     informationStudentArequipa1.style.display = "none";
@@ -335,9 +351,3 @@ wrapperScl1702.onclick = function() {
     informationStudentArequipa1.style.display = "none";
     informationStudentArequipa.style.display = "none";
 }
-
-//Funciones para mostrar gráficas
-//function myFunctionDash1() {
-//  var x = document.getElementById("d1").innerHTML;
-//document.getElementById("d1").innerHTML = '<img src="assets/images/AQP_2016-2.jpg"></img>';
-//}
